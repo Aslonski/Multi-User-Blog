3f1bba9d55289e96df5e825070938696f9fa0dfc
Aslonski <slonski.andrey@gmail.com>
2016-05-21 13:32:45 -0500

    Add login/logout functionality with bcrypt

--- app/controllers/session.rb
+++ app/controllers/session.rb
@@ -3,9 +3,17 @@ get '/login' do
 end
 
 post '/login' do
-
+  @user = User.find_by(user_name: params[:user_name])
+  if @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+    redirect "/users/#{@user.id}"
+  else
+    @errors = "Invalid username or password"
+    erb :'/login'
+  end
 end
 
 get '/logout' do
-
+  session.delete(:user_id)
+  redirect '/'
 end
--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -4,5 +4,15 @@ end
 
 
 post '/register' do
+  @user = User.new(params[:user])
+  if @user.save
+    session[:user_id] = @user.id
+    redirect '/posts'
+  else
+    redirect '/register'
+  end
+end
 
+get '/users/:id' do
+  erb :'/users/show'
 end
